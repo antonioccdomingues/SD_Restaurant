56817a007612d9ebbad40bce5a2a5fa1bb475db8
António Domingues <antonioccdomingues@hotmail.com>
2022-06-09 00:03:56 +0100

    client side entities except the waiter

--- assignment3/src/clientSide/entities/Chef.java
+++ assignment3/src/clientSide/entities/Chef.java
@@ -1,8 +1,10 @@
 package clientSide.entities;
 
-import genclass.*;
-import clientSide.stubs.BarStub;
-import clientSide.stubs.KitchenStub;
+import java.rmi.RemoteException;
+
+import commInfra.ReturnValue;
+import interfaces.kitchenInterface;
+import interfaces.barInterface;
 
 /**
  *   Chef thread.
@@ -28,13 +30,13 @@ public class Chef extends Thread
 	 *  Reference to the bar
 	 */
 
-    private BarStub bar;
+    private BarInterface bar;
 
     /**
 	 *  Reference to the Kitchen
 	 */
 
-    private KitchenStub kitchen;
+    private KitchenInterface kitchen;
 
 
     /**
@@ -46,7 +48,7 @@ public class Chef extends Thread
      *     @param bar reference to the Bar
      */
 
-    public Chef(int chefID, int state, KitchenStub kitchen, BarStub bar)
+    public Chef(int chefID, int state, KitchenInterface kitchen, BarInterface bar)
     {
         this.setChefID(chefID);
         this.state = state;
@@ -102,23 +104,23 @@ public class Chef extends Thread
     public void run()
     {
         boolean firstCourse = true;
-        kitchen.watchTheNews();
-        kitchen.startPreparation();
+        watchTheNews();
+        startPreparation();
 
         do
         {
             if(!firstCourse)
             {
-                kitchen.continuePreparation();
+                continuePreparation();
             }
             else
             {
                 firstCourse = false;
             }
-            kitchen.proceedToPresentation();
-            bar.alertTheWaiter();
+            proceedToPresentation();
+            alertTheWaiter();
 
-            while(!kitchen.haveAllPortionsBeenDelivered())
+            while(!haveAllPortionsBeenDelivered())
             {
                 kitchen.haveNextPortionReady();
                 bar.alertTheWaiter();
@@ -127,4 +129,160 @@ public class Chef extends Thread
         kitchen.cleanUp();
         GenericIO.writelnString("\033[41m Chef End Of Life \033[0m");
     }
+
+    /**
+     * transitions the Chef from the 'waiting for an order' state to the 'waiting for an order' state
+     *     
+     */
+    
+    private void watchTheNews() {
+    	ReturnValue ret = null;
+    	try
+        { ret = kitchen.watchTheNews();
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+    }
+
+    /**
+     * transitions the Chef from the 'waiting for an order' state to the 'preparing a course' state
+     *     
+     */
+
+    private void startPreparation() {
+    	ReturnValue ret = null;
+    	try
+        { ret = kitchen.startPreparation();
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+    }
+
+    /**
+     * transitions the Chef from the 'delivereing the portions' state to the 'preparing a course' state
+     *     
+     */
+
+    private void continuePreparation() {
+    	ReturnValue ret = null;
+    	try
+        { ret = kitchen.continuePreparation();
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+    }
+
+    /**
+     * transitions the Chef from the 'preparing a course' state to the 'dishing the portions' state
+     *     
+     */
+
+    private void proceedToPresentation() {
+    	ReturnValue ret = null;
+    	try
+        { ret = kitchen.proceedToPresentation();
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+    }
+
+    /**
+     * transitions the Chef from the 'dishing the portions' state to the 'delivering the portions' state and wakes the waiter
+     *     
+     */
+
+    private void alertTheWaiter() {
+    	ReturnValue ret = null;
+    	try
+        { ret = bar.alertTheWaiter();
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+    }
+
+    /**
+     * sleeps while all portions haven't been delivered the 
+     *     
+     */
+
+    private boolean haveAllPortionsBeenDelivered() {
+    	ReturnValue ret = null;
+    	try
+        { ret = kitchen.haveAllPortionsBeenDelivered();
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+        return ret.getBooleanValue();
+    }
+    
+    /**
+     * transitions the Chef from the 'delivering the portions' state to the 'dishing the portions' state
+     *     
+     */
+
+    private void haveNextPortionReady() {
+    	ReturnValue ret = null;
+    	try
+        { ret = kitchen.haveNextPortionReady();
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+    }
+
+    /**
+     * return true if the order is completed
+     *     
+     */
+
+    private boolean hasTheOrderBeenCompleted() {
+    	ReturnValue ret = null;
+    	try
+        { ret = kitchen.hasTheOrderBeenCompleted();
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+        return ret.getBooleanValue();
+    }
+
+    /**
+     * transitions the Chef from the 'delivering the portions' state to the 'closing service' state
+     *     
+     */
+
+    private void cleanUp() {
+    	ReturnValue ret = null;
+    	try
+        { ret = kitchen.cleanUp();
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+    }
+    
 }
--- assignment3/src/clientSide/entities/Student.java
+++ assignment3/src/clientSide/entities/Student.java
@@ -1,8 +1,11 @@
 package clientSide.entities;
 
-import clientSide.stubs.BarStub;
-import clientSide.stubs.TableStub;
+import java.rmi.RemoteException;
+
+import commInfra.ReturnValue;
 import clientSide.main.Constants;
+import interfaces.TableInterface;
+import interfaces.barInterface;
 
     /**
     *       Student Thread.
@@ -44,11 +47,11 @@ public class Student extends Thread
     /**
     *       Reference to the shared region "Bar"
     */
-    private BarStub bar;
+    private BarInterface bar;
     /**
     *       Reference to the shared region "Table"
     */
-    private TableStub table;
+    private TableInterface table;
 
     /**
     *   Instantiation of a Student thread.
@@ -58,7 +61,7 @@ public class Student extends Thread
     *     @param bar reference to the bar 
     *     @param table reference to the table
     */
-    public Student(int id, int state, BarStub bar, TableStub table)
+    public Student(int id, int state, BarInterface bar, TableInterface table)
     {
         this.sID=id;
         this.state = state;
@@ -183,47 +186,47 @@ public class Student extends Thread
     public void run()
     {
         walkABit();
-        bar.enter();
-        bar.readTheMenu();
+        enter();
+        readTheMenu();
 
-        if(bar.FirstStudent(this.sID))
+        if(FirstStudent(this.sID))
         {
-            table.prepareTheOrder();
-            while(!table.hasEverybodyChosen())
+            prepareTheOrder();
+            while(!hasEverybodyChosen())
             {
-                table.addUpOnesChoice();
+                addUpOnesChoice();
             }
             // Means the first student has registered everybody choice
-            bar.callTheWaiter();
-            table.describeTheOrder();
-            table.joinTheTalk();
+            callTheWaiter();
+            describeTheOrder();
+            joinTheTalk();
         }
         else
         {
-            table.informCompanion();
+            informCompanion();
         }
 
         int courses = 0;
         while(courses < Constants.courses_number)//while(!table.isOrderDone());
         {
             // wait while it's not served
-            table.waitingToBeServed(this.sID);
-            table.startEating();
-            table.endEating();
+            waitingToBeServed(this.sID);
+            startEating();
+            endEating();
             //System.out.printf("Student[%d] finshed eating\n", this.sID);
 
-            table.hasEverbodyFinished();
-            bar.signalTheWaiter(this.sID);
+            hasEverbodyFinished();
+            signalTheWaiter(this.sID);
             courses++;
         }
 
-        if(bar.shouldHaveArrivedEarlier(this.sID))
+        if(shouldHaveArrivedEarlier(this.sID))
         {
-            table.honourTheBill();
+            honourTheBill();
             System.out.printf("Student[%d] payed the bill !!!\n", this.sID);
         }
 
-        bar.exit();
+        exit();
         System.out.printf("\033[41m Student[ " + this.sID + "] End Of Life \033[0m\n");
     }    
     
@@ -250,4 +253,314 @@ public class Student extends Thread
             e.printStackTrace();
         }
     }
+
+    /**
+     * transitions the Student from the 'going to the restaurant' state to the 'taking a seat at the table' state
+     *     
+     */
+
+    private void enter() {
+    	ReturnValue ret = null;
+    	try
+        { ret = bar.enter();
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+    }
+
+    /**
+     * transitions the Student from the 'taking a seat at the table' state to the 'selecting the courses' state 
+     *     
+     */
+
+    private void readTheMenu() {
+    	ReturnValue ret = null;
+    	try
+        { ret = bar.readTheMenu();
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+    }
+
+    /**
+     * return true if is the first student to arrive at the restaurant
+     *     
+     */
+
+    private boolean FirstStudent(int sID) {
+    	ReturnValue ret = null;
+    	try
+        { ret = bar.FirstStudent(sID);
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+        return ret.getBooleanValue();
+    }
+
+    /**
+     * transitions the Student from the 'selecting the courses' state to the 'organizing the order' state 
+     *     
+     */
+
+    private void prepareTheOrder() {
+    	ReturnValue ret = null;
+    	try
+        { ret = table.prepareTheOrder();
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+    }
+
+    /**
+     * return true if all students has chosen what to eat 
+     *     
+     */
+
+    private boolean hasEverybodyChosen() {
+    	ReturnValue ret = null;
+    	try
+        { ret = table.hasEverybodyChosen();
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+        return ret.getBooleanValue();
+    }
+
+    /**
+     * transitions the Student from the 'organizing the order' state to the 'organizing the order' state untill every students select what to eat
+     *     
+     */
+
+    private void addUpOnesChoice() {
+    	ReturnValue ret = null;
+    	try
+        { ret = table.addUpOnesChoice();
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+    }
+
+    /**
+     * wake up the waiter while organizing the order
+     *     
+     */
+
+    private void callTheWaiter() {
+    	ReturnValue ret = null;
+    	try
+        { ret = bar.callTheWaiter();
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+    }  
+    
+    /**
+     * describing the order to the waiter but remaining at the same state
+     *     
+     */
+
+    private void describeTheOrder() {
+    	ReturnValue ret = null;
+    	try
+        { ret = table.describeTheOrder();
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+    }  
+
+    /**
+     * transitions the Student from the 'organizing the order' state to the 'chatting with companions' state
+     *     
+     */
+
+    private void joinTheTalk() {
+    	ReturnValue ret = null;
+    	try
+        { ret = table.joinTheTalk();
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+    }
+
+    /**
+     * transitions the Student from the 'selecting the courses' state to the 'chatting with companions' state 
+     *     
+     */
+
+    private void informCompanion() {
+    	ReturnValue ret = null;
+    	try
+        { ret = table.informCompanion();
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+    }
+
+    /**
+     * Student waites to be served
+     *     
+     */
+
+    private void waitingToBeServed(int sID) {
+    	ReturnValue ret = null;
+    	try
+        { ret = table.waitingToBeServed(sID);
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+    }
+
+    /**
+     * transitions the Student from the 'chatting with companions' state to the 'enjoying the meal' state 
+     *     
+     */
+
+    private void startEating() {
+    	ReturnValue ret = null;
+    	try
+        { ret = table.startEating();
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+    }
+
+    /**
+     * transitions the Student from the 'enjoying the meal' state to the 'chatting with companions' state 
+     *     
+     */
+
+    private void endEating() {
+    	ReturnValue ret = null;
+    	try
+        { ret = table.endEating();
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+    }
+
+    /**
+     * transitions the Student from the 'chatting with companions' state to the 'chatting with companions' state 
+     *     
+     */
+
+    private void hasEverbodyFinished() {
+    	ReturnValue ret = null;
+    	try
+        { ret = table.hasEverbodyFinished();
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+    }
+
+    /**
+     * transitions the Student from the 'chatting with companions' state to the 'chatting with companions' state and wakes up the waiter from appraising situation
+     *     
+     */
+
+    private void signalTheWaiter(int sID) {
+    	ReturnValue ret = null;
+    	try
+        { ret = bar.signalTheWaiter(sID);
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+    }
+
+    /**
+     * transitions the Student from the 'chatting with companions' state to the 'paying the bill' state and wakes up the waiter from appraising situation
+     *     
+     */
+
+    private boolean shouldHaveArrivedEarlier(int sID) {
+    	ReturnValue ret = null;
+    	try
+        { ret = bar.shouldHaveArrivedEarlier(sID);
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+        return ret.getBooleanValue();
+    }
+
+    /**
+     * Students honours the bill
+     *     
+     */
+
+    private void honourTheBill() {
+    	ReturnValue ret = null;
+    	try
+        { ret = table.honourTheBill();
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+    }
+
+    /**
+     * transitions the Student from the 'paying the bill' state to the 'going home' state 
+     *     
+     */
+
+    private void exit() {
+    	ReturnValue ret = null;
+    	try
+        { ret = bar.exit();
+        }
+        catch (RemoteException e)
+        { 
+          System.exit (1);
+        }
+    	this.state = ret.getStateValue();
+    }
+    
 }
