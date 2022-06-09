64f135248c72fc1c78757749fc6a53775854fb3c
António Domingues <antonioccdomingues@hotmail.com>
2022-06-09 15:37:31 +0100

    scripts

--- assignment3/src/RMIRegistryDeployAndRun.sh
+++ assignment3/src/RMIRegistryDeployAndRun.sh
@@ -1,13 +1,15 @@
 echo "Transfering data to the RMIregistry node."
-sshpass -p "qwerty" ssh sd106@l040101-ws09.ua.pt 'mkdir -p test/AirLift'
-sshpass -p "qwerty" ssh sd106@l040101-ws09.ua.pt 'rm -rf test/AirLift/*'
-sshpass -p "qwerty" ssh sd106@l040101-ws09.ua.pt 'mkdir -p Public/classes/interfaces'
-sshpass -p "qwerty" ssh sd106@l040101-ws09.ua.pt 'rm -rf Public/classes/interfaces/*'
-sshpass -p "qwerty" ssh sd106@l040101-ws09.ua.pt 'mkdir -p Public/classes/commInfra'
-sshpass -p "qwerty" ssh sd106@l040101-ws09.ua.pt 'rm -rf Public/classes/commInfra/*'
-sshpass -p "qwerty" scp -r genclass.jar dirRMIRegistry.zip sd106@l040101-ws09.ua.pt:/home/sd106/test/AirLift
+sshpass -p "qwerty" ssh sd205@l040101-ws09.ua.pt 'mkdir -p test/Restaurant'
+sshpass -p "qwerty" ssh sd205@l040101-ws09.ua.pt 'rm -rf test/Restaurant/*'
+sshpass -p "qwerty" ssh sd205@l040101-ws09.ua.pt 'mkdir -p Public/classes/interfaces'
+sshpass -p "qwerty" ssh sd205@l040101-ws09.ua.pt 'rm -rf Public/classes/interfaces/*'
+sshpass -p "qwerty" ssh sd205@l040101-ws09.ua.pt 'mkdir -p Public/classes/commInfra'
+sshpass -p "qwerty" ssh sd205@l040101-ws09.ua.pt 'rm -rf Public/classes/commInfra/*'
+sshpass -p "qwerty" scp -r genclass.jar dirRMIRegistry.zip sd205@l040101-ws09.ua.pt:/home/sd205/test/Restaurant
 echo "Decompressing data sent to the RMIregistry node."
-sshpass -p "qwerty" ssh -q sd106@l040101-ws09.ua.pt "cd test/AirLift ; unzip -uq dirRMIRegistry.zip"
+sshpass -p "qwerty" ssh -q sd205@l040101-ws09.ua.pt "cd test/Restaurant ; unzip -uq dirRMIRegistry.zip"
 echo "Executing program at the RMIregistry node."
-sshpass -p "qwerty" ssh sd106@l040101-ws09.ua.pt 'cd test/AirLift/dirRMIRegistry ; cp interfaces/*.class /home/sd106/Public/classes/interfaces ; cp commInfra/*.class /home/sd106/Public/classes/commInfra ; cp rmiregistry.sh /home/sd106'
-sshpass -p "qwerty" ssh -q sd106@l040101-ws09.ua.pt './rmiregistry.sh 22150'
+sshpass -p "qwerty" ssh sd205@l040101-ws09.ua.pt 'cd test/Restaurant/dirRMIRegistry ; cp interfaces/*.class /home/sd205/Public/classes/interfaces ; cp commInfra/*.class /home/sd205/Public/classes/commInfra ; cp rmiregistry.sh /home/sd205'
+sshpass -p "qwerty" ssh -q sd205@l040101-ws09.ua.pt './rmiregistry.sh 22150'
+
+
