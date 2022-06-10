e6c232dffc428375ed390ac8554967a0be747fc2
António Domingues <antonioccdomingues@hotmail.com>
2022-06-10 16:57:51 +0100

    scrip

--- assignment3/src/RegistryDeployAndRun.sh
+++ assignment3/src/RegistryDeployAndRun.sh
@@ -1,8 +1,8 @@
 echo "Transfering data to the Registry node."
-sshpass -p "qwerty" ssh sd106@l040101-ws09.ua.pt 'mkdir -p test/AirLift'
-sshpass -p "qwerty" ssh sd106@l040101-ws09.ua.pt 'rm -rf test/AirLift/*'
-sshpass -p "qwerty" scp -r genclass.jar dirRegistry.zip sd106@l040101-ws09.ua.pt:/home/sd106/test/AirLift
+sshpass -p "qwerty" ssh sd205@l040101-ws09.ua.pt 'mkdir -p test/Restaurant'
+sshpass -p "qwerty" ssh sd205@l040101-ws09.ua.pt 'rm -rf test/Restaurant/*'
+sshpass -p "qwerty" scp -r genclass.jar dirRegistry.zip sd205@l040101-ws09.ua.pt:/home/sd205/test/Restaurant
 echo "Decompressing data sent to the registry node."
-sshpass -p "qwerty" ssh -q sd106@l040101-ws09.ua.pt "cd test/AirLift ; unzip -uq dirRegistry.zip"
+sshpass -p "qwerty" ssh -q sd205@l040101-ws09.ua.pt "cd test/Restaurant ; unzip -uq dirRegistry.zip"
 echo "Executing program at the registry node."
-sshpass -p "qwerty" ssh -q sd106@l040101-ws09.ua.pt 'cd test/AirLift/dirRegistry ; ./registry.sh'
+sshpass -p "qwerty" ssh -q sd205@l040101-ws09.ua.pt 'cd test/Restaurant/dirRegistry ; ./registry.sh'
